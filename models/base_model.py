1fbe68190bfbde8a2a1ba6764a500c7d7f9a5c9f
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-03-10 17:18:06 +0300

    Added test for the dictionary]

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ This is the base class for the console
 """
 
 import uuid
-from models import storage
+# from models import storage
 from datetime import datetime
 
 
@@ -30,7 +30,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            #storage.new(self)
 
     def __str__(self):
         """ method prints human-readable string representantion
@@ -41,7 +41,7 @@ class BaseModel():
 
     def save(self):
         """ saves the current time to the update_at attribute """
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
