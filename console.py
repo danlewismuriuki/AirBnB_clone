5869e3c012e2bb426b4d3048cc65418fc8e7856e
Dan Lewis <Danlewismuriuki2@gmail.com>
2024-03-11 20:11:06 +0300

    Added the console module

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for HBNBCommand"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for HBNB project"""
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program when EOF is reached"""
+        print()
+        return True
+    
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
